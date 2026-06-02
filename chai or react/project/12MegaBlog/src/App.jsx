a2b5fe6f753d2d7ecde7ef063a24f74b5328a3fb
sahil kumar <sahilkumar06804@gmail.com>
2026-06-02 19:27:14 +0530

    MegaProject setup done!

---
+++ chai or react/project/12MegaBlog/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h2 className="text-center ">A blog app with appwrite</h2>
+    </>
+  )
+}
+
+export default App
