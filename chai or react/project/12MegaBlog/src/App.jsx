ee79e548ce908e2eae18a210013281b22cda5b51
sahil kumar <sahilkumar06804@gmail.com>
2026-06-08 15:01:29 +0530

    configure redux-toolkit

--- chai or react/project/12MegaBlog/src/App.jsx
+++ chai or react/project/12MegaBlog/src/App.jsx
@@ -1,14 +1,42 @@
-
+import {useEffect, useState} from "react"
 import './App.css'
+import { useDispatch } from "react-redux";
+import authService, { AuthService } from './appwrite/auth';
+import {login, logout} from "./store/AuthSlice"
+import { set } from "react-hook-form";
+import Header from "./components/Header/Header"
+import Footer from './components/Footer/Footer';
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h2 className="text-center ">A blog app with appwrite</h2>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+
+  },[])
+
+  return !loading ? (<div className="min-h-screen flex-wrap 
+    content-between bg-gray-400">
+      <div className="w-full block">
+        <Header/>
+        <main>
+         Todo {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>) : null
+
+
 }
 
 export default App
