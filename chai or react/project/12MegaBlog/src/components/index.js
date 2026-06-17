33595311aaa5e3845697b2d3a73c0415f08861d9
sahil kumar <sahilkumar06804@gmail.com>
2026-06-17 19:10:20 +0530

    add pages

--- chai or react/project/12MegaBlog/src/components/index.js
+++ chai or react/project/12MegaBlog/src/components/index.js
@@ -4,6 +4,10 @@ import Container from "./container/Container"
 import Logo from './Logo';
 import LogoutBtn from './Header/LogoutBtn';
 import RTE from "./RTE";
+import Signup from './Signup';
+import Login from "./Login"
+import Post from "./Post"
+import PostForm from "./post-form/PostForm"
 
 export {
     Header,
@@ -12,4 +16,8 @@ export {
     Logo,
     LogoutBtn,
     RTE,
+    Signup,
+    Login,
+    Post,
+    PostForm,
 }
